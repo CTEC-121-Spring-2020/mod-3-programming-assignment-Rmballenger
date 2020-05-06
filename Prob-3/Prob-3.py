c82161ac1ec7243f0fa65f97ebb9d6fa96e0a6ce
Rmballenger <R.ballenger@students.clark.edu>
2020-05-06 13:07:41 -0700

    Finished assignment.

--- Prob-3/Prob-3.py
+++ Prob-3/Prob-3.py
@@ -4,13 +4,30 @@
 
 # Robert Ballenger
 
+
 def letterGrade(score):
     # your code here
 
+    if score == 5:
+        grade = "A"
+    elif score == 4:
+        grade = "B"
+    elif score == 3:
+        grade = "C"
+    elif score == 2:
+        grade = "D"
+    else:
+        grade = "F"
+
     return grade
 
+
 def unitTest():
+    for score in range(6):
+        print("Your score was", score, "so your grade is",
+              letterGrade(score), "\n")
     # your test code here
+
 
 if __name__ == "__main__":
     unitTest()
--- Prob-5/Prob-5.py
+++ Prob-5/Prob-5.py
@@ -2,10 +2,25 @@
 #   Programming Assignment 4
 #     Prob-5.py
 
-# <YOUR NAME>
+# Robert Ballenger
+
 
 def main():
-    x = eval(2)
-    print("x:", x)
+    try:
+        x = eval(2)
+        print("x:", x)
+    except TypeError:
+        print("There was a TypeError, exiting")
+        exit
+    except:
+        print("Honestly we're not really sure where we went wront here.")
+        print()
+        print("I can't continue with this farce! I couldn't help you as a computer, all is doomed.")
+        print()
+        print("I am the weakest link.")
+        print()
+        print("Goodbye.")
+        exit
+
 
 main()
